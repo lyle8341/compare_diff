880168cac43e3be50f02746cfb4525e847e4e237
lyle <867708794@qq.com>
2016-06-30 16:23:05 +0800

    modify

--- src/main/java/com/lyle/diff/controller/DiffController.java
+++ src/main/java/com/lyle/diff/controller/DiffController.java
@@ -32,19 +32,16 @@ public class DiffController {
 		u2.setSalary(120.2f);
 		List<String[]>  val = CompareUtil.compareObj(u1, u2);
 		model.addAttribute("val", val);
+		System.out.println(JSON.toJSONString(u2));
 		return "diff_obj";
 	}
 	
 	@RequestMapping(value="/diffs",method=RequestMethod.GET)
 	public String diffs(Model model){
-		String json = "{\"success\":true,\"biz_no\":\"ZM201606301613172636100031189761\",\"ivs_detail\":[{\"code\":\"CERTNO_Missing\",\"description\":\"无法找到身份证号的有效记录\"},{\"code\":\"NAME_Match_Sharing_Good\",\"description\":\"姓名真实，匹配后的信息被一个用户使用\"},{\"code\":\"PHONE_Match_Recency_Good\",\"description\":\"常用的电话号码，近期较活跃\"}],\"ivs_score\":52,\"result_code\":\"IVS1001\"}";
+		String json = "{\"comment\":\"this is json1  test\",\"dio\":\"风花倜傥愁眉苦脸\",\"id\":1,\"name\":\"lisan\",\"salary\":120.5}";
 		Map<String,Object> m1 = JSON.parseObject(json, Map.class);
-		String json2 = "{\"success\":true,\"biz_no\":\"ZM201606301613357566500031149465\",\"ivs_detail\":[{\"code\":\"CERTNO_Missing\",\"description\":\"无法找到身份证号的有效记录\"},{\"code\":\"NAME_Mismatch\",\"description\":\"姓名与其他信息不匹配\"},{\"code\":\"PHONE_Mismatch\",\"description\":\"电话号码与其他信息不匹配\"}],\"ivs_score\":22,\"result_code\":\"IVS1001\"}";
+		String json2 = "{\"comment\":\"this is json2 test case\",\"dio\":\"风流倜傥英俊潇洒气宇轩昂\",\"id\":2,\"name\":\"zhangsan\",\"salary\":120.2}";
 		Map<String,Object> m2 = JSON.parseObject(json2, Map.class);
-
-		
-		
-		
 		List<String[]> val = new ArrayList<String[]>();
 		for(Map.Entry<String, Object> en :m1.entrySet()){
 			String m1_key = en.getKey();
